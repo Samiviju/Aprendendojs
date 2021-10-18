d8794f17da6615d43356e192c7f2aef4584ab08b
Samiviju <samuel.vieira.3g.com>
2021-10-18 17:58:28 -0300

    Adicionado novos arquivos de estudo

---
+++ Operadores.js
@@ -0,0 +1,13 @@
+// Operadores Aritiméticos(matemáticos)
+let salario = 100
+console.log(salario + salario);
+console.log(salario - salario);
+console.log(salario * salario);
+console.log(salario / salario);
+console.log(salario ** salario);
+
+
+// Operadores Atribuição 
+// Operadores de Comparação
+// Operadores Lógicos 
+// Operadores Bitwise 
---
+++ funcoes.js
@@ -0,0 +1,25 @@
+// Verbo + Substantivo = função
+ 
+
+let corSite = "Azul";
+
+function resetaCor(cor, tonalidade){
+    corSite = cor +  tonalidade;
+}
+console.log(corSite);
+resetaCor("verde", " claro");
+console.log(corSite);
+
+
+
+
+
+function mudaGenero(){
+
+   const sexo =  document.getElementById('#Sexo')
+   let = genero = "Masculino";
+    console.log(sexo);
+    
+}
+
+
