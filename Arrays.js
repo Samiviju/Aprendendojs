5d7b824868db442f564b1229a79ae22008574dc7
Samiviju <samuel.vieira.3g.com>
2021-10-18 15:48:12 -0300

    teste

---
+++ Arrays.js
@@ -0,0 +1,11 @@
+// Arrays - Conjunto de dados
+
+let familia = [true,52,'Ana',18];
+console.log(familia.length);
+console.log(familia);
+
+let minhasCaracteristicas = ["Samuel", 18, "Araxá", 186 ];
+console.log(minhasCaracteristicas);
+console.log(minhasCaracteristicas.length); 
+console.log(minhasCaracteristicas[2]);
+
---
+++ objetos.js
@@ -0,0 +1,37 @@
+/*let idade = 25;
+let nome = "Samuel";
+let preço = 52.4;
+ console.log(idade,nome,preço)*/
+ 
+/*const valorIngressoAdulto = 20;
+console.log(valorIngressoAdulto);*/
+
+let nome = "Samuel"
+let idade = 25
+let estarApovado = true
+let sobrenome
+
+let pessoa = {
+    nome: "Rafael",
+    estarApovado: true,
+    idade : 25,
+    sobrenome : 'Vieira',
+ };
+
+ 
+ console.log(pessoa);
+  
+ let peso = 52.4;
+ let saude = "Bem";
+ let altura = 195;
+
+ let vidaUtil = {
+    peso: 52.4,
+    saude: "Bem",
+    altura: 195,
+ };
+ console.log(vidaUtil);
+ 
+ 
+     
+ 
